a4b0d75ad69128fa7365644028915f739534476b
Dmitriy Lukash <xskilldp@gmail.com>
2021-11-02 13:57:44 +0200

    added .env file and update him. added class carstorer and save method cars to db.yml

---
+++ lib/models/car_storer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CarStorer
+  DB_CARS_PATH = ENV['DB_CARS_PATH']
+
+  class << self
+    def save(params)
+      car = Car.new(make: params[:make], model: params[:model], year: params[:year], odometer: params[:odometer],
+                    price: params[:price], description: params[:description])
+      list_cars = FileManager.read_from_yaml(path: DB_CARS_PATH)
+
+      list_cars.push(car)
+      FileManager.write_to_yaml(path: DB_CARS_PATH, data: list_cars)
+    end
+  end
+end
