19fc6a5a8e98ced39a5b122830dfbd8d5f23ca56
Dmitriy Lukash <xskilldp@gmail.com>
2021-11-09 13:31:30 +0200

    update module validator

--- lib/models/validator.rb
+++ lib/models/validator.rb
@@ -5,10 +5,10 @@ module Validator
     {
       make: args.fetch(:make, '').strip.capitalize,
       model: args.fetch(:model, '').strip.capitalize,
-      year_from: args.fetch(:year_from, '').strip.to_i,
-      year_to: args.fetch(:year_to, '').strip.to_i,
-      price_from: args.fetch(:price_from, '').strip.to_i,
-      price_to: args.fetch(:price_to, '').strip.to_i,
+      year_from: args.fetch(:year_from, 0).strip.to_i,
+      year_to: args.fetch(:year_to, 0).strip.to_i,
+      price_from: args.fetch(:price_from, 0).strip.to_i,
+      price_to: args.fetch(:price_to, 0).strip.to_i,
       sort_option: args.fetch(:sort_option, '').strip.downcase,
       sort_direction: args.fetch(:sort_direction, '').strip.downcase
     }
