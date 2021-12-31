be9793975bf65a1a8188afbb874b7ad01887136a
Dmitriy Lukash <xskilldp@gmail.com>
2021-12-31 16:20:50 +0200

    Refactoring rake task

--- lib/tasks/db.rb
+++ lib/tasks/db.rb
@@ -2,20 +2,14 @@
 
 namespace :db do
   namespace :cars do
-    desc 'Added cars to database'
-    task :create, :quantity do |_, args|
-      Rake::Task['cars:seed'].invoke
-
-      quantity = args.quantity.nil? ? 1 : args.quantity.to_i
-      cars = Helpers::FileManagerHelper.read_from_yaml(file_path: Models::Car::DB_CARS)
-
-      quantity.times do
-        cars << Models::Car.new(params).to_h
+    desc 'Create database'
+    task :create do
+      if File.exist?(Models::Car::DB_CARS)
+        puts 'Database exist.'
+      else
+        Helpers::FileManagerHelper.write_to_yaml(file_path: Models::Car::DB_CARS, data: nil)
+        puts 'Database create.'
       end
-
-      Helpers::FileManagerHelper.write_to_yaml(file_path: Models::Car::DB_CARS, data: cars)
-
-      puts "Added #{quantity} cars to db.yml"
     end
 
     desc 'Clear whole database'
@@ -24,13 +18,18 @@ namespace :db do
       puts 'Database is empty!'
     end
 
-    task :seed do
-      if File.exist?(Models::Car::DB_CARS)
-        puts 'Database exist.'
-      else
-        Helpers::FileManagerHelper.write_to_yaml(file_path: Models::Car::DB_CARS, data: nil)
-        puts 'Database create.'
+    desc 'Add cars to database'
+    task :seed, :quantity do |_, args|
+      quantity = args.quantity.nil? ? 1 : args.quantity.to_i
+      cars = Helpers::FileManagerHelper.read_from_yaml(file_path: Models::Car::DB_CARS)
+
+      quantity.times do
+        cars << Models::Car.new(car_params).to_h
       end
+
+      Helpers::FileManagerHelper.write_to_yaml(file_path: Models::Car::DB_CARS, data: cars)
+
+      puts "Added #{quantity} cars to database"
     end
 
     def car_params
